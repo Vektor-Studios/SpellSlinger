55552fac80ca3096c239a159f78e5b748307c342
Vektor-Studios <kollin.kirkman@gmail.com>
2025-09-03 12:44:31 -0500

    Pistol Rotation

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -17,7 +17,12 @@ public class Gun : MonoBehaviour
         //move to player transform
         transform.position = GameObject.FindGameObjectWithTag("Player").transform.position;
         // add an offset to the gun position
-        transform.position += new Vector3(0.5f, 0f, 0f);
+        //transform.position += new Vector3(0.5f, 0f, 0f);
+        //rotate to face mouse position using the player's transform as the rotation origin
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
 
     }
 }
